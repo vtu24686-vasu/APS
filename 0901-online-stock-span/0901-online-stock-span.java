f87db4b56ccdf1b84ebf7b9c1d4d482f39226267
vtu24686-vasu <vtu24686@veltech.edu.in>
2026-01-31 23:09:59 +0530

    Time: 30 ms (73.43%), Space: 54.6 MB (29.22%) - LeetHub

---
+++ 0901-online-stock-span/0901-online-stock-span.java
@@ -0,0 +1,20 @@
+class StockSpanner {
+
+    private Stack<int[]> st;
+
+    public StockSpanner() {
+        st = new Stack<>();
+    }
+
+    public int next(int price) {
+        int span = 1;
+
+        // Merge spans of smaller or equal prices
+        while (!st.isEmpty() && st.peek()[0] <= price) {
+            span += st.pop()[1];
+        }
+
+        st.push(new int[]{price, span});
+        return span;
+    }
+}
