ff7586b4b08dbca457e5a843dc9e19ad5ca00c98
vtu24686-vasu <vtu24686@veltech.edu.in>
2026-05-03 20:11:26 +0530

    Time: 2 ms (29.35%) | Memory: 43.9 MB (54.42%) - LeetSync

--- 926-find-and-replace-pattern/find-and-replace-pattern.java
+++ 926-find-and-replace-pattern/find-and-replace-pattern.java
@@ -3,34 +3,37 @@ import java.util.*;
 class Solution {
     public List<String> findAndReplacePattern(String[] words, String pattern) {
         List<String> result = new ArrayList<>();
-        
+
         for (String word : words) {
             if (matches(word, pattern)) {
                 result.add(word);
             }
         }
-        
+
         return result;
     }
 
     private boolean matches(String word, String pattern) {
         if (word.length() != pattern.length()) return false;
 
-        Map<Character, Character> map1 = new HashMap<>();
-        Map<Character, Character> map2 = new HashMap<>();
+        HashMap<Character, Character> map1 = new HashMap<>();
+        HashMap<Character, Character> map2 = new HashMap<>();
 
         for (int i = 0; i < word.length(); i++) {
             char w = word.charAt(i);
             char p = pattern.charAt(i);
 
-            if (map1.containsKey(p) && map1.get(p) != w)
-                return false;
+            if (map1.containsKey(w)) {
+                if (map1.get(w) != p) return false;
+            } else {
+                map1.put(w, p);
+            }
 
-            if (map2.containsKey(w) && map2.get(w) != p)
-                return false;
-
-            map1.put(p, w);
-            map2.put(w, p);
+            if (map2.containsKey(p)) {
+                if (map2.get(p) != w) return false;
+            } else {
+                map2.put(p, w);
+            }
         }
 
         return true;
