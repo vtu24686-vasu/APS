e1d00d7625e551a398cde737b878c95ff2041921
vtu24686-vasu <vtu24686@veltech.edu.in>
2026-09-03 09:56:19 +0530

    Time: 0 ms (100.00%) | Memory: 44 MB (83.94%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.java
+++ 206-reverse-linked-list/reverse-linked-list.java
@@ -10,16 +10,21 @@
  */
 class Solution {
     public ListNode reverseList(ListNode head) {
+
+
+        
         ListNode prev = null;
         ListNode curr = head;
 
         while (curr != null) {
-            ListNode next = curr.next; // save next
-            curr.next = prev;          // reverse link
-            prev = curr;               // move prev
-            curr = next;               // move curr
+            ListNode next = curr.next; // Save next node
+            curr.next = prev;          // Reverse the link
+            prev = curr;               // Move prev forward
+            curr = next;               // Move curr forward
         }
 
         return prev;
     }
 }
+
+    
