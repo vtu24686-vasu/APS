4db19e41ab850e8ef6c79612f0dcd70e82f98800
vtu24686-vasu <vtu24686@veltech.edu.in>
2026-09-03 09:25:16 +0530

    Time: 2 ms (91.84%) | Memory: 47.6 MB (76.31%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,23 +1,21 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int n=nums.length;
-        int j=-1;
-        for (int i=0; i< n; i++){
-            if (nums[i]==0){
-                j=i;
-                break;
-            }
-        }
-        if(j==-1){
-            return;
-        }
-        for (int i= j+1; i<n; i++){
-            if(nums[i]!=0){
-                int temp= nums[i]; 
-                nums[i]=nums[j];
-                nums[j]= temp;
-                j++;
+  
+
+        
+        int index = 0;
+        
+        for (int i = 0; i < nums.length; i++) {
+            
+            if (nums[i] != 0) {
+                int temp = nums[i];
+                nums[i] = nums[index];
+                nums[index] = temp;
+                
+                index++;
             }
         }
     }
 }
+ 
+    
