c0c572678a8948d7f9c8b33247d31c9c7c2f2d76
vtu24686-vasu <vtu24686@veltech.edu.in>
2026-09-03 09:26:33 +0530

    Time: 0 ms (100.00%) | Memory: 48.1 MB (74.07%) - LeetSync

--- 344-reverse-string/reverse-string.java
+++ 344-reverse-string/reverse-string.java
@@ -1,14 +1,21 @@
 class Solution {
     public void reverseString(char[] s) {
-        int n= s.length;
-        int left=0;
-        int right= n-1;
-        while (left < right){
-            char ch = s[left];
-            s[left]=s[right];
-            s[right]= ch;
+
+
+        
+        int left = 0;
+        int right = s.length - 1;
+        
+        while (left < right) {
+            
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+            
             left++;
             right--;
         }
     }
 }
+
+    
